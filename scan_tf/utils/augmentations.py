66d47044ed154201f828b49f9919c94cee241151
Robin <robin@nanovare.com>
2020-06-09 11:34:23 +0200

    Fix grey issue in rot

--- scan_tf/utils/augmentations.py
+++ scan_tf/utils/augmentations.py
@@ -1,11 +1,13 @@
 from PIL import Image, ImageEnhance, ImageOps
 import numpy as np
-import random
+
+FILLCOLOR = (0, 0, 0)
 
 def rotate_with_fill(img, magnitude):
-    rot = img.convert("RGBA").rotate(magnitude)
+    rot = img.convert("RGBA").rotate(magnitude, fillcolor=FILLCOLOR)
     return Image.composite(rot, Image.new("RGBA", rot.size, (128,) * 4), rot).convert(img.mode)
-fillcolor=0
+
+
 func_numpy = {
     "rotate": lambda img, magnitude: ndimage.rotate(img, magnitude),
     "translateX": lambda img, magnitude: ndimage.shift(img, [0, magnitude * img.shape[1] * random.choice([-1, 1]), 0]),
@@ -14,27 +16,27 @@ func_numpy = {
 
 func_pil = {
     "shearX": lambda img, magnitude: img.transform(
-        img.size, Image.AFFINE, (1, magnitude * random.choice([-1, 1]), 0, 0, 1, 0),
-        Image.BICUBIC, fillcolor=fillcolor),
+        img.size, Image.AFFINE, (1, magnitude * np.random.choice([-1, 1]), 0, 0, 1, 0),
+        Image.BICUBIC, fillcolor=FILLCOLOR),
     "shearY": lambda img, magnitude: img.transform(
-        img.size, Image.AFFINE, (1, 0, 0, magnitude * random.choice([-1, 1]), 1, 0),
-        Image.BICUBIC, fillcolor=fillcolor),
+        img.size, Image.AFFINE, (1, 0, 0, magnitude * np.random.choice([-1, 1]), 1, 0),
+        Image.BICUBIC, fillcolor=FILLCOLOR),
     "translateX": lambda img, magnitude: img.transform(
-        img.size, Image.AFFINE, (1, 0, magnitude * img.size[0] * random.choice([-1, 1]), 0, 1, 0),
-        fillcolor=fillcolor),
+        img.size, Image.AFFINE, (1, 0, magnitude * img.size[0] * np.random.choice([-1, 1]), 0, 1, 0),
+        fillcolor=FILLCOLOR),
     "translateY": lambda img, magnitude: img.transform(
-        img.size, Image.AFFINE, (1, 0, 0, 0, 1, magnitude * img.size[1] * random.choice([-1, 1])),
-        fillcolor=fillcolor),
+        img.size, Image.AFFINE, (1, 0, 0, 0, 1, magnitude * img.size[1] * np.random.choice([-1, 1])),
+        fillcolor=FILLCOLOR),
     "rotate": lambda img, magnitude: rotate_with_fill(img, magnitude),
-    "color": lambda img, magnitude: ImageEnhance.Color(img).enhance(1 + magnitude * random.choice([-1, 1])),
+    "color": lambda img, magnitude: ImageEnhance.Color(img).enhance(1 + magnitude * np.random.choice([-1, 1])),
     "posterize": lambda img, magnitude: ImageOps.posterize(img, int(magnitude)),
     "solarize": lambda img, magnitude: ImageOps.solarize(img, magnitude),
     "contrast": lambda img, magnitude: ImageEnhance.Contrast(img).enhance(
-        1 + magnitude * random.choice([-1, 1])),
+        1 + magnitude * np.random.choice([-1, 1])),
     "sharpness": lambda img, magnitude: ImageEnhance.Sharpness(img).enhance(
-        1 + magnitude * random.choice([-1, 1])),
+        1 + magnitude * np.random.choice([-1, 1])),
     "brightness": lambda img, magnitude: ImageEnhance.Brightness(img).enhance(
-        1 + magnitude * random.choice([-1, 1])),
+        1 + magnitude * np.random.choice([-1, 1])),
     "autocontrast": lambda img, magnitude: ImageOps.autocontrast(img),
     "equalize": lambda img, magnitude: ImageOps.equalize(img),
 }
