50424c28a70f9e6a4dc1275fd43c9aa123b95f2f
Robin <robin@nanovare.com>
2020-06-12 08:36:41 +0200

    Checkpoint robin

--- scan_tf/models/train.py
+++ scan_tf/models/train.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import scipy
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
 
 import scan_tf.models.resnet as resnet
 import scan_tf.utils.utils as utils
@@ -10,24 +12,31 @@ def pretext_training(backbone_model, X_train, y_train, epochs=100, save_path=Non
     lr = 1e-4
     m = X_train.shape[0]
     batch_size = 128
-
+    pretext_model = resnet.add_classification_layer(backbone_model, 4)
+    # optimizer = tf.keras.optimizers.Adam(lr=lr)
+    optimizer = tf.keras.optimizers.SGD(lr=0.1, momentum=0.9, decay=5e-4)
     for e in range(epochs):
         for batch_count in range(m // batch_size):  # batch
             batch_ids = np.random.choice(range(0, m), batch_size)
             images = tf.convert_to_tensor(X_train[batch_ids, ...])
             labels = tf.convert_to_tensor(y_train[batch_ids, ...])
             if True:
-                degrees_to_rotate = np.random.choice([90, 180, 270])  # randomly choosing the rotation angle
-                images_rotated = tf.convert_to_tensor(scipy.ndimage.rotate(images, degrees_to_rotate, axes=(2, 1)))
+                images_rotated = []
+                for image in images:
+                    images_rotated += [np.rot90(image, i) for i in [0, 1, 2, 3]]
+                images_rotated = np.array(images_rotated)
+                labels = tf.convert_to_tensor(to_categorical([0, 1, 2, 3]*batch_size))
+
+                # degrees_to_rotate = np.random.choice([0, 1, 2, 3], batch_size)  # randomly choosing the rotation angle
+                # images_rotated = np.array([np.rot90(images[i], degrees_to_rotate[i]) for i in range(len(degrees_to_rotate))])
+                # images_rotated = tf.convert_to_tensor(images_rotated)
+                # labels = tf.convert_to_tensor(to_categorical(degrees_to_rotate))
 
                 with tf.GradientTape() as tape:
-                    embs_a, embs_b = backbone_model(images), backbone_model(images_rotated)
-                    # norm = tf.map_fn(tf.nn.l2_loss, embs_a-embs_b)
-                    # loss = tf.reduce_mean(norm)
-                    loss = tf.reduce_sum(tf.norm(embs_a - embs_b, ord='euclidean', axis=(1), keepdims=None, name=None))
-                    grads = tape.gradient(loss, backbone_model.trainable_weights)
-                    optimizer = tf.keras.optimizers.Adam(lr=lr)
-                    optimizer.apply_gradients(zip(grads, backbone_model.trainable_weights))
+                    predictions = pretext_model(images_rotated)
+                    loss = tf.reduce_sum(tf.keras.losses.categorical_crossentropy(labels, predictions, from_logits=False))
+                    grads = tape.gradient(loss, pretext_model.trainable_weights)
+                    optimizer.apply_gradients(zip(grads, pretext_model.trainable_weights))
             # Disregard all this
             else:
                 images_augmented = tf.convert_to_tensor(
@@ -43,19 +52,44 @@ def pretext_training(backbone_model, X_train, y_train, epochs=100, save_path=Non
 
             if np.isnan(loss):
                 print("Nan loss found:")
-                print(embs_images.dtype, embs_images_augmented.dtype, norm.dtype, loss.dtype, grads.dtype)
-                print(embs_images[1])
-                print(embs_images_augmented[1])
                 print(f"epoch {e}, batch {batch_count} loss: {loss}")
-                return backbone_model
+                return pretext_model
 
         if e % 1 == 0:
             # clear_output()
+            print(f"{np.min(predictions)},{np.max(predictions)}")
+            print(f"{images_rotated.shape}, {labels.shape}")
             print(f"epoch {e}, loss: {loss}")
 
     if save_path is not None:
-        backbone_model.save_weights(save_path)
-    return backbone_model
+        pretext_model.save_weights(save_path)
+    return pretext_model
+
+
+def pretext_training_bis(backbone_model, X_train, y_train, epochs=100, save_path=None):
+    lr = 1e-4
+    m = X_train.shape[0]
+    batch_size = 128
+    pretext_model = resnet.add_classification_layer(backbone_model, 4)
+
+    # Prepare data
+    images_rotated = []
+    for image in X_train:
+        images_rotated += [np.rot90(image, i) for i in [0, 1, 2, 3]]
+    images_rotated = np.array(images_rotated)
+    labels = to_categorical([0, 1, 2, 3] * len(X_train))
+
+    # optimizer = tf.keras.optimizers.Adam(lr=lr)
+    optimizer = tf.keras.optimizers.SGD(lr=0.1, momentum=0.9, decay=5e-4)
+    pretext_model.compile(optimizer=optimizer,
+                          loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+                          metrics=['accuracy'],
+                          )
+    pretext_model.fit(images_rotated, labels, epochs=epochs, batch_size=batch_size, shuffle=False)
+
+    if save_path is not None:
+        pretext_model.save_weights(save_path)
+    return pretext_model
 
 
 def semantic_clustering_training(backbone_model, X_train, y_train, num_clusters=None, epochs=100, save_path=None):
